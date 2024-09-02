152337363a0ee093e2a2743a6e2ec6f2ba46700e
siddhantpurohit216 <siddhantpurohit216@gmail.com>
2024-09-03 00:01:11 +0530

    git init , added parcel pack

--- L-1(Inception)/firstJS.js
+++
@@ -1,7 +0,0 @@
-// <!-- JS code to insert elem into the root -->
-
-  const heading = document.createElement("h1");
-   heading.innerHTML = "Halooo from Javascript!!";
-   const elem = document.getElementById("root");
-  elem.appendChild(heading);
-
