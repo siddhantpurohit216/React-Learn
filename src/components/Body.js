e550d580a2f19c940d0f1b5828e7ee91176f2c8b
siddhantpurohit216 <siddhantpurohit216@gmail.com>
2024-09-18 12:44:39 +0530

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,16 @@ import Shemmer from "./shemmer";
 
 const Body = () => {
   const [restList, setRestList] = useState([]);
+  const [searchText, setSearchText] = useState([]);
+  
+  console.log("Rendedering BODY compo");
 
   useEffect(() => {
     const data = fetchData();
-    // setRestList(data);
-    // console.log(data);
   }, []);
+  
 
+  // let restaurants;
   async function fetchData() {
     const res = await fetch(
       "https://www.swiggy.com/mapi/homepage/getCards?lat=23.02760&lng=72.58710"
@@ -35,16 +38,35 @@ const Body = () => {
     console.log(restaurants);
     return restaurants;
   }
-  if(restList.length===0)return <Shemmer/>
+  if (restList.length === 0) return <Shemmer />;
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              const searchFilterdList = restList.filter((res) =>
+                res.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              console.log(searchFilterdList);
+              setRestList(searchFilterdList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            let updatedList = restList.filter(
-              (rest) => rest.avgRating > 4.5
-            );
+            let updatedList = restList.filter((rest) => rest.avgRating > 4.5);
             console.log(updatedList);
             setRestList(updatedList);
           }}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utlis/contants";
 
 const Header = () => {
+
+    const [btnName,setBtnName]=useState("Logout");
+
     return (
       <div className="header">
         <div className="logo-container">
@@ -16,6 +20,9 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button onClick={()=>{
+                if(btnName=="Logout" ? setBtnName("Login"): setBtnName("Logout") );
+            }}>{btnName}</button>
           </ul>
         </div>
       </div>
